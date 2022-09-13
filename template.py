c4e20c14b85406be0746261497acad2c7debbef4
youwol-guillaume <greinisch@youwol.com>
2022-09-13 11:10:59 +0200

    :heavy_plus_sign: add 'lz-string' dev-dependencies (prevent typedoc failure)

--- template.py
+++ template.py
@@ -33,6 +33,9 @@ template = Template(
             includedInBundle=["@typescript/vfs"]
         ),
         devTime={
+            #  those two prevent failure of typedoc
+            "@types/lz-string": "^1.3.34",
+            "lz-string": "^1.4.4",
             #  this is used only for type declarations
             'grapesjs': "0.18.3"
         }
