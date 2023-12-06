4ba3c76b04e0ed081b38e61ee869652d8e5429a5
jehadmelad <jd.melad@gmail.com>
2023-12-06 13:06:24 +0100

    🚨 eslint: set @typescript-eslint/no-explicit-any: off (we will move to next version 0.2.x and we will get rid of these lines)

--- src/lib/runner/types.ts
+++ src/lib/runner/types.ts
@@ -1,3 +1,4 @@
+/*eslint @typescript-eslint/no-explicit-any: off -- we update this version 0.1.x => 0.2.x where we do not use these lines anymore*/
 export type CdnClient = {
     install: (p, p2?) => Promise<Window>
     LoadingScreenView: any
